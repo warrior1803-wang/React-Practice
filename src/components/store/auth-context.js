cde8fc86b58df3d974f4f650cc31adcf1cfd6686
warrior1803-wang <60429051+warrior1803-wang@users.noreply.github.com>
2022-04-17 17:15:05 +0800

    contextapi

--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -1,7 +1,32 @@
-import React from 'react'
-
-const AuthContext =  React.createContext({
-    isLoggedIn:false
+import React,{useState,useEffect} from 'react';
+const AuthContext=React.createContext({
+    isLoggedIn:false,
+    onLogout:()=>{},
+    onLogin:(email,password)=>{}
 });
+export const AuthContextProvider = (props)=>{
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+      };
+      const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+      };
+      
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
 
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+    
+    return <AuthContext.Provider value={{isLoggedIn:isLoggedIn,onLogout:logoutHandler,onLogin:loginHandler}}>
+        {props.children}
+    </AuthContext.Provider>
+}
 export default AuthContext;
